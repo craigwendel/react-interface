398853ccc2cf59c1360e045d26e1438d625a78a7
Craig Wendel <wendelcraig@gmail.com>
2018-02-18 11:05:34 -0600

    Initial commit. Create folders for components and styles, rendering correctly in test environment.

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+import '../styles/App.css'
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <nav className="navbar navbar-default">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <a className="navbar-brand" href="#">
+                <img alt="Brand" src="..." />
+              </a>
+            </div>
+          </div>
+        </nav>
+      </div>
+    )
+  }
+}
+
+export default App
