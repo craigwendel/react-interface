bc750e326e2cb804d430366b3c6601113f37ad0c
Craig Wendel <wendelcraig@gmail.com>
2018-02-19 12:46:54 -0600

    Modified application to render the App component only, delete MainInterface component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,46 @@
 import React, { Component } from 'react'
 import '../styles/App.css'
-import MainInterface from './MainInterface'
 
-class App extends Component {
+import data from '../data/data.js'
+
+import AptList from './AptList'
+import AddApt from './AddApt'
+
+let _ = require('lodash')
+
+export default class App extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      myAppointments: data,
+      aptBodyVisible: false
+    }
+  }
+
+  deleteMessage = (item) => {
+    let allApts = this.state.myAppointments
+    let newApts = _.without(allApts, item)
+    this.setState({myAppointments: newApts})
+  }
+
+  toggleAddDisplay = () => {
+    this.setState({aptBodyVisible: !this.state.aptBodyVisible})
+  }
+
   render () {
+    let filteredApts = this.state.myAppointments
+    filteredApts = filteredApts.map((item, i) => {
+      return (
+        <AptList key={i} item={item} whichItem={item} onDelete={this.deleteMessage} />
+      )
+    })
+
     return (
-      <div className='app'>
-        <MainInterface />
+      <div className='interface'>
+        <AddApt bodyVisible={this.state.aptBodyVisible} handleToggle={this.toggleAddDisplay} />
+        <ul className='item-list media-list'>{filteredApts}</ul>
       </div>
     )
   }
 }
-
-export default App
